9983e78a562b5cb44f100668d5576e8d486151a5
irtizaf <irtizaf@yahoo.com>
2023-09-30 15:45:14 -0700

    commit

--- lib/demohello-stack.ts
+++ lib/demohello-stack.ts
@@ -2,22 +2,18 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from "aws-cdk-lib/aws-lambda"
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ddb from 'aws-cdk-lib/aws-dynamodb';
 
 export class DemohelloStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-
-    const lambda1  = new lambda.Function(this, 'MyLambda', {
-      code: lambda.Code.fromAsset('lambda'),
-      handler: 'index.handler',
-      runtime: lambda.Runtime.NODEJS_16_X,
+    const productTable = new ddb.Table(this, 'Table', {
+      tableName:"TestTable",
+      partitionKey: {
+        name: 'id',
+        type: ddb.AttributeType.STRING,
+      },
     });
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'DemohelloQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
